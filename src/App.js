b24fc2f5aa2a4639d469626b3deec33f9c29ff1f
gausu60 <gausu60@gmail.com>
2022-08-19 10:57:48 +0530

    todo-list web application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [task, setTask] = useState([]);
+  function handleChange(event) {
+    const newTask = event.target.value;
+    setInputText(newTask);
+  }
+  function handleSubmit(event) {
+    setTask((prevTask) => {
+      return [...prevTask, inputText];
+    });
+    setInputText("");
+    event.preventDefault();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={handleSubmit}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {task.map((todoItem) => (
+            <li>{todoItem}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
-
 export default App;
